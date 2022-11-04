0d6de09c68750ebed5b8acc39c11e6c11bc5d9e6
canoztas <59293768+canoztas@users.noreply.github.com>
2022-11-04 11:25:01 +0300

    Update uif.py

--- uif.py
+++ uif.py
@@ -2,21 +2,22 @@ import sys
 import re
 import numpy as np
 
+# get filename
 filename = sys.argv[1]
 
 # opening and reading the file 
 with open(filename) as fh:
    fstring = fh.readlines()
   
-# declaring the regex pattern for IP addresses
+# regex pattern of IP addresses
 pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
   
-# initializing the list object
+# empty list object
 lst=[]
   
 # extracting the IP addresses
 for line in fstring:
    lst.append(pattern.search(line)[0])
   
-# displaying the extracted IP addresses
+# displaying count of the unique IP addresses
 print(len(np.unique(lst)))
